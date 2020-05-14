3381c6fbd71145a1c1ddeea7daef5b3753875221
SammySupreme B <samb9382@gmail.com>
2020-05-14 09:39:39 -0500

    created game, computer, human, and player classes for RPSLS project

--- RPSLS/Computer.cs
+++ RPSLS/Computer.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    class Program
+    class Computer
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    class Program
+    class Game
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
